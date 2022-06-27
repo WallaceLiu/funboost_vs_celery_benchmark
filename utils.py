d3cec740ab386fa3626359fa0c86341c009a3aed
liuning <liuning@bjxy.com>
2022-06-27 11:20:27 +0800

    init

--- utils.py
+++ utils.py
@@ -1,19 +1,17 @@
-"""
-
-
-5000*10000=50000000
-
-
-
-"""
-
 import pandas as pd
 import numpy as np
 from toolz import partition_all
 
 
-start = pd.to_datetime('2022-6-25 13:41:11')
-end = pd.to_datetime('2022-6-25 13:42:06')
-print(end - start)
+def get_second_from(start, end):
+    """
 
+    :param start:  2022-6-25 13:41:11
+    :param end:  2022-6-25 13:42:06
+    :return:
+    """
+    start = pd.to_datetime(start)
+    end = pd.to_datetime(end)
+    r = end - start
+    return r.seconds
 
---
+++ utils_test.py
@@ -0,0 +1,3 @@
+from utils import *
+
+print('%ss' % get_second_from('2022-6-25 13:41:11', '2022-6-25 13:42:06'))
